d532dc0aabf78a8508bb6fff9f9d1ae89d59fde3
Jakub Furtak <kuba.furtak.pl@gmail.com>
2022-08-13 14:18:03 +0200

    added list of apps

---
+++ app/src/main/java/com/bignerdranch/android/nerdlauncher/NerdLauncherActivity.java
@@ -0,0 +1,13 @@
+package com.bignerdranch.android.nerdlauncher;
+
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+
+public class NerdLauncherActivity extends SingleFragmentActivity {
+
+    @Override
+    protected Fragment createFragment() {
+        return NerdLauncherFragment.newInstance();
+    }
+}
